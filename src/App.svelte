b79d56dafce236f6d3ae88ff96dc1c3e665ffd1a
alex <camward@yandex.ru>
2019-07-29 22:00:37 +0400

    переменные

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let name;
+	let name = 'Local H1'
+	let src = 'https://ru.svelte.dev/svelte-logo-horizontal.svg'
+	let text = 'Local H2'
+	let htmlText = '<b>Strong</b> text'
 </script>
 
 <style>
 	h1 {
 		color: purple;
 	}
+	:global(h2) {
+		color: green;
+	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>{name}!</h1>
+<img src="{src}" alt="{text}" />
+<h2>{text.toUpperCase()}!</h2>
+<p>{@html htmlText}</p>
