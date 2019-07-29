08d4eff9c115de2a2872c12861812cf26d518f8b
alex <camward@yandex.ru>
2019-07-29 22:31:18 +0400

    binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,14 @@
 <script>
 	let name = 'Svelte'
-	let counter = 0
-
-	$: upperName = name.toUpperCase()
-
-	$: counterClass = counter % 2 === 0 ? 'red' : 'green'
-
-	function changeNameHandler() {
-		name = 'Canged name'
-	}
+	let check = true
 </script>
 
 <style>
 	h1 {
 		color: purple;
 	}
-	.red {
-		color: red;
-	}
-	.green {
-		color: green;
-	}
 </style>
 
 <h1>{name}!</h1>
-<h2>{upperName}!</h2>
-<button on:click="{changeNameHandler}">Change name</button>
-
-<h1 class="{counterClass}">Counter {counter}</h1>
-<button on:click="{() => counter++}">Add +1</button>
+<input type="text" bind:value="{name}" />
+<input type="checkbox" bind:checked="{check}" />
